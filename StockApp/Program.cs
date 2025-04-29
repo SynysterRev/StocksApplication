96f16a62d633435f98c383658dfa9dc1907cb065
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-04-29 09:54:51 +0200

    Add exception middlware to display custom error page

--- StockApp/Program.cs
+++ StockApp/Program.cs
@@ -1,27 +1,25 @@
 using Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Repositories;
 using RepositoryContracts;
+using Serilog;
 using ServiceContracts;
 using Services;
 using StockApp;
+using StockApp.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
-builder.Services.AddHttpClient();
-builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
 
-builder.Services.AddScoped<IStocksService, StocksService>();
-builder.Services.AddScoped<IFinnhubService, FinnhubService>();
-builder.Services.AddScoped<IFinnhubRepository, FinnhubRepository>();
-builder.Services.AddScoped<IStocksRepository, StocksRepository>();
+//builder.Logging.ClearProviders().AddConsole().AddDebug();
 
-
-if (!builder.Environment.IsEnvironment("Test"))
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider services, LoggerConfiguration loggerConfiguration) =>
 {
-    builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-}
+    loggerConfiguration.ReadFrom.Configuration(context.Configuration) //read configuration settings from build int config (appsettings)
+    .ReadFrom.Services(services); //read out current app's services and make them available to the serilog
+});
+
+builder.Services.ConfigureServices(builder);
 
 var app = builder.Build();
 
@@ -29,6 +27,17 @@ if (!builder.Environment.IsEnvironment("Test"))
 {
     Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", "Rotativa");
 }
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandlingMiddleware();
+}
+
 
 app.UseStaticFiles();
 app.UseRouting();
---
+++ StockApp/StartupExtensions/ConfigureServicesExtension.cs
@@ -0,0 +1,29 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using RepositoryContracts;
+using ServiceContracts;
+using Services;
+
+namespace StockApp
+{
+    public static class ConfigureServicesExtension
+    {
+        public static void ConfigureServices(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddHttpClient();
+            builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
+            builder.Services.AddScoped<IStocksService, StocksService>();
+            builder.Services.AddScoped<IFinnhubService, FinnhubService>();
+            builder.Services.AddScoped<IFinnhubRepository, FinnhubRepository>();
+            builder.Services.AddScoped<IStocksRepository, StocksRepository>();
+
+            if (!builder.Environment.IsEnvironment("Test"))
+            {
+                builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            }
+        }
+    }
+}
